7d51bac67f64d9573f6dc7388dc7180a3b351c81
RicardoSaid <said_s027@hotmail.es>
2017-11-02 17:09:53 -0600

    I put the bottons in different order

--- maintk.py
+++ maintk.py
@@ -3,21 +3,27 @@ from tkinter import *
 class App():
     def __init__(self, master):
 
-        self.frame = Frame(master)
+        self.frame = Frame(master, height =100 , width = 100)
+        self.frame.propagate(0)
         self.frame.pack()
-        self.button = Button(self.frame, text = "QUI", fg = "RED", state = DISABLED)
-        self.button.pack(side = LEFT)
+        self.button = Button(self.frame, text = "QUI", fg = "RED", command = self.frame.quit)
+        self.button.pack(fill=BOTH, expand = 1)
+        self.button.pack(side = RIGHT)
 
         self.hi_there = Button(self.frame, text = "HELLO",fg = "green", command = self.say_hi)
-        self.hi_there.pack(side= LEFT)
+        self.hi_there.pack(fill=BOTH, expand = 1)
+        self.hi_there.pack(side = LEFT)
 
-        butoon = Button(text = "OK", command = self.callback)
-        butoon.pack(side = RIGHT)
+        self.butoon = Button(self.frame, text = "OK", command = self.callback)
+        self.butoon.pack(fill = BOTH, expand = 1)
+        self.butoon.pack(side = TOP)
+        #self.butoon.pack(side = RIGHT)
 
     def say_hi(self):
         print("Hi there, everyone")
     def callback(self):
         print("click!")
+
 
 root = Tk()
 app = App(root)

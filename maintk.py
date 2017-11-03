856ffcaae04811290c1c140deb7c16b43f2fa5ee
RicardoSaid <said_s027@hotmail.es>
2017-11-02 22:39:18 -0600

    I used canvas for create lines and a rectangle

--- maintk.py
+++ maintk.py
@@ -3,7 +3,7 @@ from tkinter import *
 class App():
     def __init__(self, master):
 
-        self.frame = Frame(master, height =100 , width = 100)
+        self.frame = Frame(master, height =100 , width = 1000)
         self.frame.propagate(0)
         self.frame.pack()
         self.button = Button(self.frame, text = "QUI", fg = "RED", command = self.frame.quit, compound = CENTER)
@@ -16,6 +16,15 @@ class App():
 
         self.butoon = Button(self.frame, text = "OK", command = self.callback,compound = CENTER)
         self.butoon.pack(fill = BOTH, expand = 1)
+
+        self.view = Canvas(master,width = 200, height = 200)
+        self.view.pack()
+        self.view.create_line(0,0,200,100)
+        self.view.create_line(0,100,200,0, fill = "red", dash = (44,44))
+        self.view.create_rectangle(0,100,200,0, fill = "blue")
+
+        self.other_line = self.view.create_line(0,0,50,50, fill = "orange")
+
 
     def say_hi(self):
         print("Hi there, everyone")
